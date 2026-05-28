4da3ebae585d58021007ed02f6195410043114c0
PC <PC@DESKTOP-UIGH3EF>
2026-05-28 16:33:14 -0500

    Agregar archivos de proyecto.

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace StretchFilmApp
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.bttnSol = new System.Windows.Forms.Button();
+            this.bttnCotiz = new System.Windows.Forms.Button();
+            this.bttnProv = new System.Windows.Forms.Button();
+            this.bttnProd = new System.Windows.Forms.Button();
+            this.bttnAdqui = new System.Windows.Forms.Button();
+            this.bttnFac = new System.Windows.Forms.Button();
+            this.bttnCobra = new System.Windows.Forms.Button();
+            this.bttnMarg = new System.Windows.Forms.Button();
+            this.bttnUser = new System.Windows.Forms.Button();
+            this.bttnClien = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bttnSol
+            // 
+            this.bttnSol.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnSol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnSol.Location = new System.Drawing.Point(32, 60);
+            this.bttnSol.Name = "bttnSol";
+            this.bttnSol.Size = new System.Drawing.Size(100, 35);
+            this.bttnSol.TabIndex = 0;
+            this.bttnSol.Text = "Solicitudes";
+            this.bttnSol.UseVisualStyleBackColor = false;
+            // 
+            // bttnCotiz
+            // 
+            this.bttnCotiz.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnCotiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnCotiz.Location = new System.Drawing.Point(32, 120);
+            this.bttnCotiz.Name = "bttnCotiz";
+            this.bttnCotiz.Size = new System.Drawing.Size(100, 35);
+            this.bttnCotiz.TabIndex = 1;
+            this.bttnCotiz.Text = "Cotizaciones";
+            this.bttnCotiz.UseVisualStyleBackColor = false;
+            // 
+            // bttnProv
+            // 
+            this.bttnProv.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnProv.Location = new System.Drawing.Point(32, 180);
+            this.bttnProv.Name = "bttnProv";
+            this.bttnProv.Size = new System.Drawing.Size(100, 35);
+            this.bttnProv.TabIndex = 2;
+            this.bttnProv.Text = "Proveedores";
+            this.bttnProv.UseVisualStyleBackColor = false;
+            // 
+            // bttnProd
+            // 
+            this.bttnProd.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnProd.Location = new System.Drawing.Point(32, 240);
+            this.bttnProd.Name = "bttnProd";
+            this.bttnProd.Size = new System.Drawing.Size(100, 35);
+            this.bttnProd.TabIndex = 3;
+            this.bttnProd.Text = "Productos";
+            this.bttnProd.UseVisualStyleBackColor = false;
+            // 
+            // bttnAdqui
+            // 
+            this.bttnAdqui.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnAdqui.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnAdqui.Location = new System.Drawing.Point(32, 300);
+            this.bttnAdqui.Name = "bttnAdqui";
+            this.bttnAdqui.Size = new System.Drawing.Size(100, 35);
+            this.bttnAdqui.TabIndex = 4;
+            this.bttnAdqui.Text = "Adquisicion";
+            this.bttnAdqui.UseVisualStyleBackColor = false;
+            // 
+            // bttnFac
+            // 
+            this.bttnFac.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnFac.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnFac.Location = new System.Drawing.Point(32, 360);
+            this.bttnFac.Name = "bttnFac";
+            this.bttnFac.Size = new System.Drawing.Size(100, 35);
+            this.bttnFac.TabIndex = 5;
+            this.bttnFac.Text = "Facturacion";
+            this.bttnFac.UseVisualStyleBackColor = false;
+            // 
+            // bttnCobra
+            // 
+            this.bttnCobra.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnCobra.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnCobra.Location = new System.Drawing.Point(32, 420);
+            this.bttnCobra.Name = "bttnCobra";
+            this.bttnCobra.Size = new System.Drawing.Size(100, 35);
+            this.bttnCobra.TabIndex = 6;
+            this.bttnCobra.Text = "Cobranza";
+            this.bttnCobra.UseVisualStyleBackColor = false;
+            // 
+            // bttnMarg
+            // 
+            this.bttnMarg.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnMarg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnMarg.Location = new System.Drawing.Point(32, 480);
+            this.bttnMarg.Name = "bttnMarg";
+            this.bttnMarg.Size = new System.Drawing.Size(100, 35);
+            this.bttnMarg.TabIndex = 7;
+            this.bttnMarg.Text = "Margenes";
+            this.bttnMarg.UseVisualStyleBackColor = false;
+            // 
+            // bttnUser
+            // 
+            this.bttnUser.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnUser.Location = new System.Drawing.Point(32, 540);
+            this.bttnUser.Name = "bttnUser";
+            this.bttnUser.Size = new System.Drawing.Size(100, 35);
+            this.bttnUser.TabIndex = 8;
+            this.bttnUser.Text = "Usuarios";
+            this.bttnUser.UseVisualStyleBackColor = false;
+            // 
+            // bttnClien
+            // 
+            this.bttnClien.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bttnClien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnClien.Location = new System.Drawing.Point(32, 600);
+            this.bttnClien.Name = "bttnClien";
+            this.bttnClien.Size = new System.Drawing.Size(100, 35);
+            this.bttnClien.TabIndex = 9;
+            this.bttnClien.Text = "Clientes";
+            this.bttnClien.UseVisualStyleBackColor = false;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(814, 788);
+            this.Controls.Add(this.bttnClien);
+            this.Controls.Add(this.bttnUser);
+            this.Controls.Add(this.bttnMarg);
+            this.Controls.Add(this.bttnCobra);
+            this.Controls.Add(this.bttnFac);
+            this.Controls.Add(this.bttnAdqui);
+            this.Controls.Add(this.bttnProd);
+            this.Controls.Add(this.bttnProv);
+            this.Controls.Add(this.bttnCotiz);
+            this.Controls.Add(this.bttnSol);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnSol;
+        private System.Windows.Forms.Button bttnCotiz;
+        private System.Windows.Forms.Button bttnProv;
+        private System.Windows.Forms.Button bttnProd;
+        private System.Windows.Forms.Button bttnAdqui;
+        private System.Windows.Forms.Button bttnFac;
+        private System.Windows.Forms.Button bttnCobra;
+        private System.Windows.Forms.Button bttnMarg;
+        private System.Windows.Forms.Button bttnUser;
+        private System.Windows.Forms.Button bttnClien;
+    }
+}
