404285656d7f843135869faddd2f7ca6f4ec15e2
Jss-G9 <aegis.noc@outlook.com>
2026-06-07 20:40:22 -0500

    prueba-facturacion

--- Facturación.Designer.cs
+++ Facturación.Designer.cs
@@ -28,12 +28,318 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvFacturas = new System.Windows.Forms.DataGridView();
+            this.colSerie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVendedora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSaldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMargen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEstado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVence = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTotalFacturado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblPorCobrar = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblVencido = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCantidadFacturas = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvFacturas
+            // 
+            this.dgvFacturas.AllowUserToAddRows = false;
+            this.dgvFacturas.AllowUserToDeleteRows = false;
+            this.dgvFacturas.AllowUserToResizeColumns = false;
+            this.dgvFacturas.AllowUserToResizeRows = false;
+            this.dgvFacturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFacturas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSerie,
+            this.colCliente,
+            this.colVendedora,
+            this.colTipo,
+            this.colTotal,
+            this.colSaldo,
+            this.colMargen,
+            this.colEstado,
+            this.colVence});
+            this.dgvFacturas.Location = new System.Drawing.Point(15, 199);
+            this.dgvFacturas.MultiSelect = false;
+            this.dgvFacturas.Name = "dgvFacturas";
+            this.dgvFacturas.RowHeadersVisible = false;
+            this.dgvFacturas.Size = new System.Drawing.Size(1012, 426);
+            this.dgvFacturas.TabIndex = 15;
+            // 
+            // colSerie
+            // 
+            this.colSerie.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colSerie.DataPropertyName = "Serie";
+            this.colSerie.HeaderText = "SERIE";
+            this.colSerie.Name = "colSerie";
+            this.colSerie.Width = 85;
+            // 
+            // colCliente
+            // 
+            this.colCliente.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colCliente.DataPropertyName = "Cliente";
+            this.colCliente.HeaderText = "CLIENTE";
+            this.colCliente.Name = "colCliente";
+            this.colCliente.Width = 180;
+            // 
+            // colVendedora
+            // 
+            this.colVendedora.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colVendedora.DataPropertyName = "Vendedora";
+            this.colVendedora.HeaderText = "VENDEDORA";
+            this.colVendedora.Name = "colVendedora";
+            this.colVendedora.Width = 130;
+            // 
+            // colTipo
+            // 
+            this.colTipo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colTipo.DataPropertyName = "Tipo";
+            this.colTipo.HeaderText = "TIPO";
+            this.colTipo.Name = "colTipo";
+            this.colTipo.Width = 110;
+            // 
+            // colTotal
+            // 
+            this.colTotal.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colTotal.DataPropertyName = "Total";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.Format = "C2";
+            this.colTotal.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colTotal.HeaderText = "TOTAL";
+            this.colTotal.Name = "colTotal";
+            // 
+            // colSaldo
+            // 
+            this.colSaldo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colSaldo.DataPropertyName = "Saldo";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.Format = "C2";
+            this.colSaldo.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colSaldo.HeaderText = "SALDO";
+            this.colSaldo.Name = "colSaldo";
+            // 
+            // colMargen
+            // 
+            this.colMargen.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colMargen.DataPropertyName = "Margen";
+            dataGridViewCellStyle3.Format = "P0";
+            this.colMargen.DefaultCellStyle = dataGridViewCellStyle3;
+            this.colMargen.HeaderText = "MARGEN";
+            this.colMargen.Name = "colMargen";
+            // 
+            // colEstado
+            // 
+            this.colEstado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colEstado.DataPropertyName = "Estado";
+            this.colEstado.HeaderText = "ESTADO";
+            this.colEstado.Name = "colEstado";
+            // 
+            // colVence
+            // 
+            this.colVence.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.colVence.DataPropertyName = "Vence";
+            dataGridViewCellStyle4.Format = "dd/MM/yyy";
+            this.colVence.DefaultCellStyle = dataGridViewCellStyle4;
+            this.colVence.HeaderText = "VENCE";
+            this.colVence.Name = "colVence";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(761, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Nueva Factura";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 93);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(858, 70);
+            this.flowLayoutPanel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.lblTotalFacturado);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(277, 63);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblTotalFacturado
+            // 
+            this.lblTotalFacturado.AutoSize = true;
+            this.lblTotalFacturado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalFacturado.Location = new System.Drawing.Point(78, 33);
+            this.lblTotalFacturado.Name = "lblTotalFacturado";
+            this.lblTotalFacturado.Size = new System.Drawing.Size(126, 13);
+            this.lblTotalFacturado.TabIndex = 4;
+            this.lblTotalFacturado.Text = "TOTAL FACTURADO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TOTAL FACTURADO";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.lblPorCobrar);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(286, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(277, 63);
+            this.panel3.TabIndex = 6;
+            // 
+            // lblPorCobrar
+            // 
+            this.lblPorCobrar.AutoSize = true;
+            this.lblPorCobrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorCobrar.Location = new System.Drawing.Point(95, 33);
+            this.lblPorCobrar.Name = "lblPorCobrar";
+            this.lblPorCobrar.Size = new System.Drawing.Size(88, 13);
+            this.lblPorCobrar.TabIndex = 4;
+            this.lblPorCobrar.Text = "POR COBRAR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "POR COBRAR";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblVencido);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(569, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 63);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblVencido
+            // 
+            this.lblVencido.AutoSize = true;
+            this.lblVencido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVencido.Location = new System.Drawing.Point(111, 33);
+            this.lblVencido.Name = "lblVencido";
+            this.lblVencido.Size = new System.Drawing.Size(62, 13);
+            this.lblVencido.TabIndex = 4;
+            this.lblVencido.Text = "VENCIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "VENCIDO";
+            // 
+            // lblCantidadFacturas
+            // 
+            this.lblCantidadFacturas.AutoSize = true;
+            this.lblCantidadFacturas.Location = new System.Drawing.Point(12, 58);
+            this.lblCantidadFacturas.Name = "lblCantidadFacturas";
+            this.lblCantidadFacturas.Size = new System.Drawing.Size(105, 13);
+            this.lblCantidadFacturas.TabIndex = 12;
+            this.lblCantidadFacturas.Text = "Cantidad de facturas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "FACTURACIÓN";
+            // 
+            // Facturación
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1046, 682);
+            this.Controls.Add(this.dgvFacturas);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblCantidadFacturas);
+            this.Controls.Add(this.label1);
+            this.Name = "Facturación";
             this.Text = "Facturación";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvFacturas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSerie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVendedora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSaldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMargen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEstado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVence;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTotalFacturado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblPorCobrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblVencido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCantidadFacturas;
+        private System.Windows.Forms.Label label1;
     }
 }
